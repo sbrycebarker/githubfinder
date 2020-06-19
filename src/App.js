d3f2fb45e204d4acc4df5f3b77eecb78e18e68db
sbrycebarker <sbrycesti@gmail.com>
2020-06-19 16:15:56 -0600

    users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return <div className="App">
       <Navbar />
+      <Users />
       </div>
   }
 }
