15a268f779401adb60012fe648e7310c5416a780
sbrycebarker <sbrycesti@gmail.com>
2020-06-23 19:39:22 -0600

    secret and id

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const Navbar = ( {icon, title} ) => { 
+const Navbar = ( { icon, title } ) => { 
         return (
             <nav className='navbar bg-primary'>
                <h1>
