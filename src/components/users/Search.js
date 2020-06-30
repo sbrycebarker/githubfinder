0f259e2dfce478932c40fcdd486e920f993a20fe
sbrycebarker <sbrycesti@gmail.com>
2020-06-30 17:14:49 -0600

    ...

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
     };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
 
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value })
@@ -11,7 +16,8 @@ export class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text)
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
     }
     render() {
         return (
